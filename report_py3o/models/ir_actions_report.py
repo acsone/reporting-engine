8285df3c7025034b6b7d06634a22d7716e55431c
Arnaud Pineux <arnaud.pineux@acsone.eu>
2021-06-29 16:13:21 +0200

    [IMP] report_py3o: black, isort, prettier

--- report_py3o/models/ir_actions_report.py
+++ report_py3o/models/ir_actions_report.py
@@ -20,7 +20,7 @@ PY3O_CONVERSION_COMMAND_PARAMETER = "py3o.conversion_command"
 
 
 class IrActionsReport(models.Model):
-    """ Inherit from ir.actions.report to allow customizing the template
+    """Inherit from ir.actions.report to allow customizing the template
     file. The user cam chose a template from a list.
     The list is configurable in the configuration tab, see py3o_template.py
     """
@@ -169,8 +169,7 @@ class IrActionsReport(models.Model):
         )
 
     def gen_report_download_filename(self, res_ids, data):
-        """Override this function to change the name of the downloaded report
-        """
+        """Override this function to change the name of the downloaded report"""
         self.ensure_one()
         report = self.get_from_report_name(self.report_name, self.report_type)
         if report.print_report_name and not len(res_ids) > 1:
@@ -179,8 +178,7 @@ class IrActionsReport(models.Model):
         return "{}.{}".format(self.name, self.py3o_filetype)
 
     def _get_attachments(self, res_ids):
-        """ Return the report already generated for the given res_ids
-        """
+        """Return the report already generated for the given res_ids"""
         self.ensure_one()
         save_in_attachment = {}
         if res_ids:
