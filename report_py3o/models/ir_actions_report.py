89d20ce265c23b184df40ee547d528538c18e524
Arnaud Pineux <arnaud.pineux@acsone.eu>
2021-06-29 16:48:32 +0200

    [FIX] report_type: required selection fields must define an ondelete policy that implements the proper cleanup

--- report_py3o/models/ir_actions_report.py
+++ report_py3o/models/ir_actions_report.py
@@ -65,7 +65,10 @@ class IrActionsReport(models.Model):
             "or an absolute path on your server."
         ),
     )
-    report_type = fields.Selection(selection_add=[("py3o", "Py3o")])
+    report_type = fields.Selection(
+        selection_add=[("py3o", "Py3o")],
+        ondelete={'py3o': 'cascade'},
+    )
     py3o_multi_in_one = fields.Boolean(
         string="Multiple Records in a Single Report",
         help="If you execute a report on several records, "
