16404bfeb9f4103b1cdfb25a4c0a01202eee220f
Laurent-Corron <laurent.corron@acsone.eu>
2021-06-30 15:45:24 +0200

    [ADD] report_py3o_signer

--- report_qweb_signer/models/ir_actions_report.py
+++ report_qweb_signer/models/ir_actions_report.py
@@ -30,9 +30,15 @@ def _normalize_filepath(path):
 class IrActionsReport(models.Model):
     _inherit = "ir.actions.report"
 
+    def _check_valid_action_report(self):
+        if self.report_type != "qweb-pdf":
+            return True
+        return False
+
     def _certificate_get(self, res_ids):
         """Obtain the proper certificate for the report and the conditions."""
-        if self.report_type != "qweb-pdf":
+        valid = self._check_valid_action_report()
+        if not valid:
             return False
         certificates = self.env["report.certificate"].search(
             [
