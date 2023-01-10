7ed4a351b12330284c99ce2a95c4118cf6cb50d0
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2023-01-10 16:43:09 +0100

    [REF] report_py3o: python 3.11 compatibility

--- report_py3o/models/ir_actions_report.py
+++ report_py3o/models/ir_actions_report.py
@@ -2,11 +2,12 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import logging
+import time
 
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.tools.misc import find_in_path
-from odoo.tools.safe_eval import safe_eval, time
+from odoo.tools.safe_eval import safe_eval
 
 logger = logging.getLogger(__name__)
 
